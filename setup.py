b114753ccd78ef4a1a21a6632456282238248c90
Antony Holmes <antony.b.holmes@gmail.com>
2019-06-03 12:14:46 -0400

    Updates and bug fixes.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='libgff',
-    version='0.1.0',
+    version='0.2.0',
     author='Antony B Holmes',
     author_email='antony.b.holmes@gmail.com',
     description='A library for handling GTF/GFF/GFF3 files',
